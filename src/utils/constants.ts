a57a5c54864fc6eec75393f1859bc6e434eaebbb
sergio-cravas <sergio.cravas@gmail.com>
2025-07-12 20:11:33 +0200

    fix: :bug: Update email address in constants to correct value

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -2,4 +2,4 @@ export const GITHUB_URL = "https://github.com/sergio-cravas";
 
 export const LINKEDIN_URL = "https://www.linkedin.com/in/sergio-cerda-hervas/";
 
-export const EMAIL_URL = 'mailto:sergio.cerdah@gmail.com'
+export const EMAIL_URL = 'mailto:sergio.cravas@gmail.com'
